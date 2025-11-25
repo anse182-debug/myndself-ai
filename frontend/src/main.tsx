59f981f42f2a01dd90c08f87c248bc9eb57665bd
anse182-debug <anse182.aa@gmail.com>
2025-11-25 22:54:33 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+// src/main.tsx
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import App from "./App"
+import Landing from "./Landing"
+import "./styles.css"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )

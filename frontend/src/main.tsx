46e9c11a97c6aec62d74f57f6da3fa821ddb0d63
anse182-debug <anse182.aa@gmail.com>
2025-12-07 17:18:18 +0100

    Add TermsPage and PrivacyPage routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 // src/main.tsx
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import App from "./App"
-import Landing from "./Landing"
-import "./styles.css"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from "./App";
+import Landing from "./Landing";
+import TermsPage from "./Terms";    // 👈 AGGIUNGI QUESTO
+import PrivacyPage from "./Privacy"; // 👈 AGGIUNGI QUESTO
+
+import "./styles.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -12,7 +16,9 @@ createRoot(document.getElementById("root") as HTMLElement).render(
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/app" element={<App />} />
+        <Route path="/terms" element={<TermsPage />} />     {/* 👈 QUI */}
+        <Route path="/privacy" element={<PrivacyPage />} /> {/* 👈 QUI */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
